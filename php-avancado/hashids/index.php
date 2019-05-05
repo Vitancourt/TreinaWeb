7714fbab9d8c9c3b79d7588594f6b32bd73676c1
Maikel <maikel-93@hotmail.com>
2019-05-05 09:46:54 -0300

    Hashids

--- php-avancado/hashids/index.php
+++ php-avancado/hashids/index.php
@@ -1,6 +1,14 @@
 <?php
 
-require __DIR__.'..vendor/autoload.php';
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 
-$hashids = new Hashids\Hashids();
+require __DIR__.'../../vendor/autoload.php';
 
+$hashids = new Hashids\Hashids(
+    'VMS^@?%M-agD9$2e',
+    '10'
+);
+
+echo $hashids->encode(1);
